17e50aa9dba33c634ad8fda07c30ed7170d381a9
Puddingboy <ruben.damink@gmail.com>
2023-10-03 16:16:16 +0200

    Update Controller.php
    make type_template override feed_template

--- src/Controller.php
+++ src/Controller.php
@@ -21,17 +21,36 @@ class Controller extends ExtensionController
             'content_length' => $config['content_length'],
         ];
 
-        if ($type === 'rss') {
-            $template = '@rss-extension/rss.xml.twig';
-            $headerContentType = 'application/rss+xml;charset=UTF-8';
-        } elseif ($type === 'atom') {
-            $template = '@rss-extension/atom.xml.twig';
-            $headerContentType = 'application/atom+xml;charset=UTF-8';
-        } else {
-            $template = '@rss-extension/json.twig';
-            $headerContentType = 'application/json';
+        if (!empty($config['feed_template'])) {
+            $template = $config['feed_template'];
         }
 
+        if ($type === 'rss') {
+            if (!isset($template)) {
+                $template = '@rss-extension/rss.xml.twig';
+            }
+            if (!empty($config['rss_template'])) {
+                $template = $config['rss_template'];
+            }
+            $headerContentType = 'application/rss+xml;charset=UTF-8';
+        } elseif ($type === 'atom') {
+            if (!isset($template)) {
+                $template = '@rss-extension/atom.xml.twig';
+            }
+            if (!empty($config['atom_template'])) {
+                $template = $config['atom_template'];
+            }
+            $headerContentType = 'application/atom+xml;charset=UTF-8';
+        } else {
+            if (!isset($template)) {
+                $template = '@rss-extension/json.twig';
+            }
+            if (!empty($config['json_template'])) {
+                $template = $config['json_template'];
+            }
+            $headerContentType = 'application/json';
+        }
+        
         $response = $this->render($template, $context);
         $response->headers->set('Content-Type', $headerContentType);
 
